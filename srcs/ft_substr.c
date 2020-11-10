d9258a59dfb57ca3914f1367518b669cf878f181
Augustin Debureaux <adebureaux@gmail.com>
2020-11-10 19:59:11 +0100

    Fix substr

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 00:14:35 by adeburea          #+#    #+#             */
-/*   Updated: 2020/09/28 20:43:27 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/11/10 19:57:53 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char *dst;
 
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s) < len ? ft_strlen(s) : len;
 	while (start-- && *s)
 		s++;
 	if (!(dst = (char*)malloc(sizeof(char) * len + 1)))
