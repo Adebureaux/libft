e8aa4f2b7039575c08b2cceb7f8227ac2cc1bc7d
Augustin Debureaux <adebureaux@gmail.com>
2020-11-17 16:38:04 +0100

    Put header

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 17:29:05 by adeburea          #+#    #+#             */
-/*   Updated: 2020/11/13 02:17:34 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/11/17 16:30:07 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	dst = NULL;
 	while (lst && f)
 	{
-		if (!(elem = ft_lstnew(f(&lst->content))))
+		if (!(elem = ft_lstnew(f(lst->content))))
 		{
 			ft_lstclear(&dst, del);
 			break ;
