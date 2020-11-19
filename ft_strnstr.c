c1232d76c502a26c98802dfa62263a4a2281f1ca
Augustin Debureaux <adebureaux@gmail.com>
2020-11-19 05:02:51 +0100

    Rework completly strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 00:14:14 by adeburea          #+#    #+#             */
-/*   Updated: 2020/11/14 13:47:33 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/11/19 04:56:39 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,25 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t nee_len;
+	size_t i;
+	size_t j;
 
-	if (!*needle || haystack == needle)
+	i = 0;
+	if (!*needle)
 		return ((char*)haystack);
 	if (len < 1)
 		return (NULL);
-	nee_len = ft_strlen(needle);
-	while (*haystack && --len > nee_len)
+	while (haystack[i] && i < len)
 	{
-		if (*haystack == *needle && ft_memcmp(haystack, needle, nee_len) == 0)
-			return ((char*)haystack);
-		haystack++;
+		j = 0;
+		while (haystack[i] && haystack[i] == needle[j] && i < len)
+		{
+			j++;
+			i++;
+		}
+		if (!needle[j])
+			return ((char*)haystack + (i - j));
+		i++;
 	}
 	return (NULL);
 }
