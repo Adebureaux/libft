975f46c8955a0549a00b0b9f82b3a5f062a3c4c2
Augustin Debureaux <adebureaux@gmail.com>
2020-11-20 01:01:33 +0100

    Protect malloc breaking issues

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,28 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 00:13:00 by adeburea          #+#    #+#             */
-/*   Updated: 2020/11/19 23:02:51 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/11/20 00:59:24 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	**ft_free_split(char **dst, int *char_nbr)
+{
+	int i;
+
+	i = 0;
+	if (char_nbr)
+		free(char_nbr);
+	if (dst)
+	{
+		ft_bzero(dst, sizeof(char*));
+		while (dst[i])
+			free(dst[i++]);
+		free(dst);
+	}
+	return (NULL);
+}
 
 int		ft_split_strs(const char *str, char c)
 {
@@ -36,8 +53,6 @@ int		*ft_split_chars(const char *str, char c, int str_nbr)
 	int		i;
 
 	i = 0;
-	if (!*str)
-		return (0);
 	if (!(char_nbr = (int*)malloc(sizeof(int) * str_nbr)))
 		return (NULL);
 	while (str_nbr--)
@@ -64,7 +79,10 @@ char	**ft_spliter(const char *str, char c, int *char_nbr, int str_nbr)
 	i = -1;
 	j = 0;
 	if (!(dst = (char**)malloc(sizeof(char*) * (str_nbr + 1))))
+	{
 		return (NULL);
+	}
+
 	while (++i < str_nbr)
 	{
 		while (str[j] == c)
@@ -72,7 +90,7 @@ char	**ft_spliter(const char *str, char c, int *char_nbr, int str_nbr)
 		while (str[j] && str[j] != c)
 		{
 			if (!(dst[i] = ft_substr(str, j, char_nbr[i])))
-				return (NULL);
+				return (ft_free_split(dst, char_nbr));
 			j += char_nbr[i];
 		}
 	}
@@ -89,8 +107,10 @@ char	**ft_split(const char *s, char c)
 	if (!s)
 		return (NULL);
 	str_nbr = ft_split_strs(s, c);
-	char_nbr = ft_split_chars(s, c, str_nbr);
-	dst = ft_spliter(s, c, char_nbr, str_nbr);
+	if (!(char_nbr = ft_split_chars(s, c, str_nbr)))
+		return (NULL);
+	if (!(dst = ft_spliter(s, c, char_nbr, str_nbr)))
+		return (ft_free_split(dst, char_nbr));
 	free(char_nbr);
 	return (dst);
 }
